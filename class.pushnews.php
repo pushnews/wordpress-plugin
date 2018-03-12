b22b1947b004dd3a90cf582c87843f6bb9dcb53a
Mário Balça <mario.balca@gmail.com>
2018-03-12 12:40:15 +0000

    Added a validation to check auth_token before hooking post

--- class.pushnews.php
+++ class.pushnews.php
@@ -106,13 +106,16 @@ MYHTML;
 				$notification['message']['icon'] = get_the_post_thumbnail_url($post);
 			}
 
-			wp_remote_post("https://api.pushnews.eu/v2/push/" . $options['app_id'], array(
-				"body" => json_encode($notification),
-				"headers" => array(
-					'X-Auth-Token' => $options['auth_token'],
-					"Content-Type" => "application/json"
-                )
-			));
+			if(isset($options['auth_token'])) {
+				//wp_remote_post("https://api.pushnews.eu/v2/push/" . $options['app_id'], array(
+                wp_remote_post("http://local.app.pushnews.eu/api.php/v2/push/" . $options['app_id'], array(
+					"body" => json_encode($notification),
+					"headers" => array(
+						'X-Auth-Token' => $options['auth_token'],
+						"Content-Type" => "application/json"
+					)
+				));
+			}
 		}
 	}
 
