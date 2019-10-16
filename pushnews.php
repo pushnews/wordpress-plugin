dc0bf6f2f7ea7b922345dacbe63fb757f6096b6e
tixastronauta <tiago.carvalho@gmail.com>
2019-10-16 18:41:09 +0100

    - Added WooCommerce integration allowing for abandoned cart recovery Push Notification - Redesigned admin interface

--- pushnews.php
+++ pushnews.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
-* Plugin Name:        Pushnews
-* Author:             Pushnews <developers@pushnews.eu>
-* Plugin URI:         https://www.pushnews.eu/
-* Description:        Send Web Push Notifications to your visitors. Increase your website traffic - Simple and fast UI - Automate push notifications via Facebook Page integration.
-* Version:            1.10.2
-* Author URI:         https://www.pushnews.eu/
-* License:            GPLv2 or later
-* Text Domain:        pushnews
-* Domain Path:        /languages
-**/
+ * Plugin Name:        Pushnews
+ * Author:             Pushnews <developers@pushnews.eu>
+ * Plugin URI:         https://www.pushnews.eu/
+ * Description:        Send Web Push Notifications to your visitors. Increase your website traffic - Simple and fast UI - Automate push notifications via Facebook Page integration.
+ * Version:            2.0.0
+ * Author URI:         https://www.pushnews.eu/
+ * License:            GPLv2 or later
+ * Text Domain:        pushnews
+ * Domain Path:        /languages
+ **/
 
 /*
 This program is free software; you can redistribute it and/or
@@ -28,24 +28,32 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-define( 'PUSHNEWS_VERSION', '1.10.2' );
+define( 'PUSHNEWS_VERSION', '2.0.0' );
 define( 'PUSHNEWS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 require_once( PUSHNEWS__PLUGIN_DIR . 'class.pushnews.php' );
 require_once( PUSHNEWS__PLUGIN_DIR . 'class.pushnewsbase64url.php' );
 
+// pushnews admin styles
 add_action( 'admin_enqueue_scripts', array( 'Pushnews', 'admin_styles' ) );
 
+// initialize plugin
 add_action( 'init', array( 'Pushnews', 'init' ) );
 add_action( 'plugins_loaded', array( 'Pushnews', 'translations_init' ) );
 
+// woo-commerce integration
+add_action( 'woocommerce_add_to_cart', array( 'Pushnews', 'woocommerce_add_to_cart' ) );
+add_action( 'woocommerce_thankyou', array( 'Pushnews', 'woocommerce_thankyou' ) );
+
+// plugin installation events
 register_activation_hook( __FILE__, array( 'Pushnews', 'plugin_activation' ) );
 register_deactivation_hook( __FILE__, array( 'Pushnews', 'plugin_deactivation' ) );
 register_uninstall_hook( __FILE__, array( 'Pushnews', 'plugin_uninstall' ) );
 
+// inject pushnews js tag
 add_action( 'wp_footer', array( 'Pushnews', 'inject_tag' ) );
 
-// Send push on post publish
-add_action( 'add_meta_boxes', array( 'Pushnews',  'add_custom_meta_box' ));
-add_action( 'future_post', array('Pushnews', 'future_post_custom_hook' ), 10, 1);
-add_action( 'save_post', array('Pushnews', 'save_post_custom_hook' ), 10, 3 );
+// send push on post publish
+add_action( 'add_meta_boxes', array( 'Pushnews', 'add_custom_meta_box' ) );
+add_action( 'future_post', array( 'Pushnews', 'future_post_custom_hook' ), 10, 1 );
+add_action( 'save_post', array( 'Pushnews', 'save_post_custom_hook' ), 10, 3 );
